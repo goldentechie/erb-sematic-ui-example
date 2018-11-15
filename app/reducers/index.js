9ed38e25d77e2021893cd8f58135bc3e20920362
goldentechie <bendam1207@gmail.com>
2018-11-15 01:05:27 -0800

    Changed Devtron package to dev-dependency (#2004)

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,9 +3,10 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import counter from './counter';
 
-export default function createRootReducer(history: History) {
-  return combineReducers({
-    router: connectRouter(history),
-    counter
-  });
+export default function createRootReducer(history: {}) {
+  const routerReducer = connectRouter(history)(() => {});
+
+  return connectRouter(history)(
+    combineReducers({ router: routerReducer, counter })
+  );
 }
