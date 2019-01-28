9a09648726dd684a8c59394ece04102600d370d4
goldentechie <bendam1207@gmail.com>
2019-01-27 16:58:38 -0800

    initial semantic ui integration

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,29 @@
 // @flow
+/* eslint jsx-a11y/label-has-associated-control: off, jsx-a11y/label-has-for: off */
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Checkbox, Form } from 'semantic-ui-react';
 import routes from '../constants/routes';
 import styles from './Home.css';
 
 type Props = {};
+
+const FormExampleForm = () => (
+  <Form>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder="First Name" />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder="Last Name" />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label="I agree to the Terms and Conditions" />
+    </Form.Field>
+    <Button type="submit">Submit</Button>
+  </Form>
+);
 
 export default class Home extends Component<Props> {
   props: Props;
@@ -13,6 +32,8 @@ export default class Home extends Component<Props> {
     return (
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
+        This form is using Semantic UI!
+        <FormExampleForm />
         <Link to={routes.COUNTER}>to Counter</Link>
       </div>
     );
